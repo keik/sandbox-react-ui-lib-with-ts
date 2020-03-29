b0baf6147974b28d026efeff6053251fa417bef7
keik <keik@c-fo.com>
2020-03-29 23:20:20 +0900

    :+1: Improve interface.

--- src/Button.tsx
+++ src/Button.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as _ from "styled-components/cssprop";
 
 type Props = {
-  primary: boolean;
+  primary?: true;
 };
 export const Button: React.FC<Props> = ({ children, primary }) => (
   <button
