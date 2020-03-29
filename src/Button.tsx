b3e50f8862fc0d8d26d0c27f4e5d6be2d1731f6e
keik <keik@c-fo.com>
2020-03-29 21:53:16 +0900

    :+1: Boilerplating...

---
+++ src/Button.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import * as _ from 'styled-components/cssprop'
+
+type Props = {
+    primary: boolean;
+}
+export const Button: React.FC<Props> = ({children,primary}) => <button css={{
+    backgroundColor: 'white',
+    color: 'gray',
+    ...(primary &&{
+        backgroundColor: 'blue',
+        color:'white'
+    })
+}}>{children}</button>
