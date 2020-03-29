9c85b793d2decec2284bdcd010ac8672f61eacb7
keik <keik@c-fo.com>
2020-03-30 00:48:29 +0900

    :+1: Organize settings.

--- babel.config.cjs.js
+++ babel.config.cjs.js
@@ -1,14 +1,4 @@
 module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          browsers: ["ie >= 10"],
-        },
-      },
-    ],
-    "@babel/preset-react",
-  ],
+  presets: ["@babel/preset-env", "@babel/preset-react"],
   plugins: ["babel-plugin-styled-components"],
 };
